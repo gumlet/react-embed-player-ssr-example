88546cb2a6a1854a71605cad0ed96033c4a00768
Bruce Wayne <muditsjain@gmail.com>
2025-06-04 16:10:53 +0530

    removed unused code

--- src/App.js
+++ src/App.js
@@ -57,37 +57,37 @@ const App = () => {
               videoID="64bfb0913ed6e5096d66dc1e"
               title="Gumlet Player Example"
               style={{ height: '100vh', width: '100vw', position: 'relative' }}
-            schemaOrgVideoObject={{
-              "@context": "https://schema.org",
-              "@type": "VideoObject",
-              "name": "Siren pink 3:4 video",
-              "description": "",
-              "thumbnailUrl": [
-                ["https://video.gumlet.io/603cc6a9926fb6233baebb34/66487345e3dcc416dc9bbb5b/thumbnail-1-0.png?v=1716024171990"]
-              ],
-              "uploadDate": "2024-05-18T09:22:13.592Z",
-              "duration": "PT7.833333S",
-              "embedUrl": "https://play.gumlet.io/embed/66487345e3dcc416dc9bbb5b"
-            }}
-            autoplay={false}
-            preload={false}
-            muted={true}
-            disable_player_controls={false}
-            t={35}
-            onReady={() => console.log("Player is ready.")}
-            onPlay={() => console.log("Video is playing.")}
-            onPause={() => console.log("Video is paused.")}
-            onProgress={(e) => console.log("Video is at", e.percent)}
-            onTimeUpdate={(e) => console.log("Video is at", e.seconds, "seconds")}
-            onEnded={() => console.log("Video has ended.")}
-            onFullScreenChange={(e) => console.log("Full screen change", e)}
-            onPipChange={(e) => console.log("Picture in picture", e)}
-            onAudioChange={(e) => console.log("Audio changed", e)}
-            onQualityChange={(e) => console.log("Quality changed", e)}
-            onVolumeChange={(e) => console.log("Volume changed", e)}
-            onSeeked={(e) => console.log("Seeked", e)}
-            onError={(e) => console.log("Error", e)}
-          />
+              schemaOrgVideoObject={{
+                "@context": "https://schema.org",
+                "@type": "VideoObject",
+                "name": "Siren pink 3:4 video",
+                "description": "",
+                "thumbnailUrl": [
+                  ["https://video.gumlet.io/603cc6a9926fb6233baebb34/66487345e3dcc416dc9bbb5b/thumbnail-1-0.png?v=1716024171990"]
+                ],
+                "uploadDate": "2024-05-18T09:22:13.592Z",
+                "duration": "PT7.833333S",
+                "embedUrl": "https://play.gumlet.io/embed/66487345e3dcc416dc9bbb5b"
+              }}
+              autoplay={false}
+              preload={false}
+              muted={true}
+              disable_player_controls={false}
+              t={35}
+              onReady={() => console.log("Player is ready.")}
+              onPlay={() => console.log("Video is playing.")}
+              onPause={() => console.log("Video is paused.")}
+              onProgress={(e) => console.log("Video is at", e.percent)}
+              onTimeUpdate={(e) => console.log("Video is at", e.seconds, "seconds")}
+              onEnded={() => console.log("Video has ended.")}
+              onFullScreenChange={(e) => console.log("Full screen change", e)}
+              onPipChange={(e) => console.log("Picture in picture", e)}
+              onAudioChange={(e) => console.log("Audio changed", e)}
+              onQualityChange={(e) => console.log("Quality changed", e)}
+              onVolumeChange={(e) => console.log("Volume changed", e)}
+              onSeeked={(e) => console.log("Seeked", e)}
+              onError={(e) => console.log("Error", e)}
+            />
           </>
         ) : (
           <div>Loading player...</div>
